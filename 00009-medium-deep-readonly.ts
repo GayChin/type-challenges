8881231d42d686c6a0417ae29e1433c6a73561ab
GayChin <gcyap1227@gmail.com>
2023-07-06 17:13:29 +0800

    solve 9

---
+++ 00009-medium-deep-readonly.ts
@@ -0,0 +1,71 @@
+// ============= Test Cases =============
+import type { Equal, Expect } from './test-utils'
+
+type cases = [
+  Expect<Equal<DeepReadonly<X1>, Expected1>>,
+  Expect<Equal<DeepReadonly<X2>, Expected2>>,
+]
+
+type X1 = {
+  a: () => 22
+  b: string
+  c: {
+    d: boolean
+    e: {
+      g: {
+        h: {
+          i: true
+          j: 'string'
+        }
+        k: 'hello'
+      }
+      l: [
+        'hi',
+        {
+          m: ['hey']
+        },
+      ]
+    }
+  }
+}
+
+type X2 = { a: string } | { b: number }
+
+type Expected1 = {
+  readonly a: () => 22
+  readonly b: string
+  readonly c: {
+    readonly d: boolean
+    readonly e: {
+      readonly g: {
+        readonly h: {
+          readonly i: true
+          readonly j: 'string'
+        }
+        readonly k: 'hello'
+      }
+      readonly l: readonly [
+        'hi',
+        {
+          readonly m: readonly ['hey']
+        },
+      ]
+    }
+  }
+}
+
+type Expected2 = { readonly a: string } | { readonly b: number }
+
+
+// ============= Your Code Here =============
+type DeepReadonly<T> = {
+  readonly [ K in keyof T] : keyof T[K] extends never 
+   ? T[K] 
+   : DeepReadonly<T[K]> 
+}
+
+
+// ============= Explaination =============
+// keyof T[K] extends never checks if the keys of the type T[K] extend the 'never' type
+// keyof T[K] reperesents union of keys of type T[K]
+// If it extends 'never', it means the type is a primitive type that do not have further value or properties
